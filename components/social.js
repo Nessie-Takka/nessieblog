6db06b2b92f9463a90f75d4f3ba7597c6e9c9767
Nessie-Takka <lovemyself0603@outlook.jp>
2024-03-01 10:39:38 +0900

    SNSのリンクを追加

--- components/social.js
+++ components/social.js
@@ -2,28 +2,14 @@ import styles from 'styles/social.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-    faFacebookF,
-    faXTwitter,
     faGithub,
 } from '@fortawesome/free-brands-svg-icons'
 
 export default function Social(iconSize = 'initial') {
     return (
-        <ul className={styles.list} style={{'--icon-size':iconSize}}>
+        <ul className={styles.list} style={{ '--icon-size': iconSize }}>
             <li>
-                <a href='https://twitter.com'>
-                    <FontAwesomeIcon icon={faXTwitter} />
-                    <span className='sr-only'>X</span>
-                </a>
-            </li>
-            <li>
-                <a href='https://facebook.com'>
-                    <FontAwesomeIcon icon={faFacebookF} />
-                    <span className='sr-only'>Facebook</span>
-                </a>
-            </li>
-            <li>
-                <a href='https://github.com'>
+                <a href='https://github.com/Nessie-Takka'>
                     <FontAwesomeIcon icon={faGithub} />
                     <span className='sr-only'>Github</span>
                 </a>
